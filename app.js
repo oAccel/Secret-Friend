ee7c1d01f729ccdc99caf6ea74111ea4a9226d8c
Gabriel <gabrielbomfimpestana@outlook.com>
2025-02-02 04:24:05 -0300

    First

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosLista = [];
+
+
+function adicionarAmigo() {
+    let nomes = document.getElementById("amigos");
+    amigosLista.push(nomes);
+    console.log(amigosLista);
+}
+
+
+
+function limpar() {
+    nome
+}
